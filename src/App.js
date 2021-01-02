db8e4813b99234761bfdf5e83caf0d9d4bb4f0bd
niemet0502 <vincentmarius8@gmail.com>
2021-01-02 23:35:06 +0100

    main :: added add todo feature

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import {Provider} from "react-redux"
 import store from "./store"
 import {TodoListStore} from "./component/TodoList"
+import {AddTodoForm} from "./component/AddTodo"
 function App() {
   return (
     <Provider  store={store}>
       <TodoListStore />
+      <AddTodoForm />
     </Provider>
   );
 }
--- src/component/AddTodo.jsx
+++ src/component/AddTodo.jsx
@@ -1,14 +1,20 @@
-import React from "react"
+import React, {useRef} from "react"
 import {useDispatch} from "react-redux"
-
+import {addTodoAction} from "../store/todosAction"
 
 export function AddTodoForm(){
     const dispatch = useDispatch() 
     const input = useRef(null)
 
-    
+    const handleSubmit = e => {
+        e.preventDefault()
+        dispatch(addTodoAction(input.current.value))
+        input.current.value = ''
+        input.current.focus()
+    }
 
     return <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Tache" ref={input}/>
+        <button>Ajouter</button>
     </form>
 }
